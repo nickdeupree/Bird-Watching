25602a887dbc4d667217d268e2cf3c4e6f18a56c
ananyabatra <ananyabatra04@gmail.com>
2024-11-15 17:41:55 -0800

    trying to display map

--- apps/bird-watching/static/js/index.js
+++ apps/bird-watching/static/js/index.js
@@ -1,24 +1,35 @@
 "use strict";
 
+
 // This will be the object that will contain the Vue attributes
 // and be used to initialize it.
 let app = {};
 
-
-app.data = {    
-    data: function() {
+app.data = {
+    data: function () {
         return {
             // Complete as you see fit.
             my_value: 1, // This is an example.
+            center: {  lat: -34.397, lng: 150.644 }
         };
     },
     methods: {
         // Complete as you see fit.
-        my_function: function() {
+        my_function: function () {
             // This is an example.
             this.my_value += 1;
         },
-    }
+
+        initMap: async function () {
+            const { Map } = await google.maps.importLibrary("maps");
+
+            this.map = new Map(document.getElementById("map"), {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8,
+            });
+        }
+
+    },
 };
 
 app.vue = Vue.createApp(app.data).mount("#app");
---
+++ node_modules/vue3-google-map/dist/types/components/AdvancedMarker.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="google.maps" />
+import { PropType } from "vue";
+declare const _default: import("vue").DefineComponent<{
+    options: {
+        type: PropType<google.maps.marker.AdvancedMarkerElementOptions>;
+        required: true;
+    };
+    pinOptions: {
+        type: PropType<google.maps.marker.PinElementOptions>;
+        required: false;
+    };
+}, () => import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    [key: string]: any;
+}>[] | undefined, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, string[], string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    options: {
+        type: PropType<google.maps.marker.AdvancedMarkerElementOptions>;
+        required: true;
+    };
+    pinOptions: {
+        type: PropType<google.maps.marker.PinElementOptions>;
+        required: false;
+    };
+}>> & {
+    [x: `on${Capitalize<string>}`]: ((...args: any[]) => any) | undefined;
+}, {}, {}>;
+export default _default;
