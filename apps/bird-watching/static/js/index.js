408366dc7e4f9d81f1a747357ad0d71769d6ad5a
ananyabatra <ananyabatra04@gmail.com>
2024-11-20 10:01:40 -0800

    map renders properly

--- apps/bird-watching/static/js/index.js
+++ apps/bird-watching/static/js/index.js
@@ -1,78 +1,52 @@
 "use strict";
 
-
 // This will be the object that will contain the Vue attributes
 // and be used to initialize it.
-
-const MapComponent = {
-    template: `
-        <div id="map" style="height: 500px; width: 100%;"></div>
-    `,
-    data() {
-        return {
-            map: null, // The Leaflet map instance
-        };
-    },
-    methods: {
-        click_listener(e) {
-            console.log("Map clicked at", e.latlng);
-        },
-        dbclick_listener(e) {
-            console.log("Map double-clicked at", e.latlng);
-        },
-    },
-    mounted() {
-        this.map = L.map("map").setView([51.505, -0.09], 13);
-
-        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        }).addTo(this.map);
-
-        this.map.on("click", this.click_listener);
-        this.map.on("dblclick", this.dbclick_listener);
-    },
-    beforeUnmount() {
-        if (this.map) {
-            this.map.off("click", this.click_listener);
-            this.map.off("dblclick", this.dbclick_listener);
-            this.map.remove();
-        }
-    },
-};
-
 let app = {};
 
-app.data = {
-    data: function () {
-        return {
-            // Complete as you see fit.
-            my_value: 1, // This is an example.
-        };
-    },
-    methods: {
-        // Complete as you see fit.
-        my_function: function () {
-            // This is an example.
-            this.my_value += 1;
-        }
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+    app.data = {
+        user_email: null,
+        is_drawing: false,
+        map: null,
+    };
 
-    },
+    app.enumerate = (a) => {
+        let k = 0;
+        a.map((e) => { e._idx = k++; });
+        return a;
+    };
 
-    components: {
-        MapComponent
-    }
+    app.methods = {
+        toggle_drawing() {
+            this.is_drawing = !this.is_drawing;
+        },
+    };
+
+    app.vue = Vue.createApp({
+        data() {
+            return app.data;
+        },
+        methods: app.methods,
+        mounted() {
+            // Add a timeout to initialize the map after 2 seconds
+            setTimeout(() => {
+                this.map = L.map("map").setView([51.505, -0.09], 13);
+                L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                }).addTo(this.map);
+
+                this.map.invalidateSize();
+            }, 100); // adding slight delay to help with rendering
+        },
+    });
+
+    // Mount the Vue app to the target element.
+    app.vue.mount("#vue-target");
 };
 
-
-
-app.vue = Vue.createApp(app.data).mount("#app");
-
-app.load_data = function () {
-    axios.get(my_callback_url).then(function (r) {
-        app.vue.my_value = r.data.my_value;
-    });
-}
-
-app.load_data();
-
+// This takes the (empty) app object, and initializes it.
+init(app);
