4711c176243c22c033cdd85a90e14f68fa19f382
Anusha <anushapai1316@gmail.com>
2024-12-08 21:23:05 -0800

    Removed sp. suffix from species names and added styling to list of species based on user

--- apps/bird-watching/static/js/stats.js
+++ apps/bird-watching/static/js/stats.js
@@ -22,7 +22,11 @@ app.vue = Vue.createApp(app.data).mount("#app");
 app.load_data = function () {
     axios.get(load_user_stats_url).then(function (r) {
         app.vue.user_email = r.data.user_email;
-        app.vue.species_list = r.data.species_list;
+        // app.vue.species_list = r.data.species_list;
+        app.vue.species_list = r.data.species_list.map(function(species) {
+            species.COMMON_NAME = species.COMMON_NAME.replace(/\s*sp\.$/, ''); // Regex to match " sp."
+            return species;
+        });
         console.log('stats are ', app.vue.species_list);
         console.log('stats should be ', r.data.species_list);
     });
