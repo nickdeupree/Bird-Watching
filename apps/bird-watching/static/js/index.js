a3d97b794ed7a0f13b7b35b3f07338f1a58883ce
ananyabatra <ananyabatra04@gmail.com>
2024-12-09 21:19:01 -0800

    changed center

--- apps/bird-watching/static/js/index.js
+++ apps/bird-watching/static/js/index.js
@@ -194,7 +194,7 @@ let init = (app) => {
                     });
 
                 setTimeout(() => {
-                    self.map = L.map("map").setView([36.98, -121.98], 13);
+                    self.map = L.map("map").setView([37.4, -122], 8.5);
                     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
                         maxZoom: 19,
                         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
