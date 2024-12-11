dba140ee3cf6660758c7644a4b9dc377b0177696
Nikita Shenoy <nmshenoy@ucsc.edu>
2024-12-11 01:19:05 -0800

    fixed backend lowercase/uppercase bug and added descriptive comments to code

--- apps/bird-watching/static/js/checklist.js
+++ apps/bird-watching/static/js/checklist.js
@@ -20,14 +20,14 @@ app.data = {
             observation_time: null,
             duration: null,
             center: null,
-            valid_time: true,
-            valid_date: true,
+            valid_time: true, // flag for valid time
+            valid_date: true, // flag for valid date
         };
     },
     methods: {
-        // Complete as you see fit.
         add_species: function () {
             let self = this;
+            // missing species name check
             self.missing_species_name = false;
             if (self.new_species.trim() === "") {
                 console.log("Species name was not inputted");
@@ -35,6 +35,7 @@ app.data = {
                 return;
             }
 
+            // fixing quantity format
             if (self.initial_quantity == null) {
                 self.initial_quantity = 1;
             }
@@ -43,6 +44,7 @@ app.data = {
                 (item) => item.species_name.toLowerCase() === this.new_species.trim().toLowerCase()
             );
 
+            // if species already exists in checklist, update quantity, else add to checklist
             if (idx !== -1) {
                 console.log("Updating quantity");
                 self.update_quantity(idx, self.initial_quantity - self.checklist[idx].OBSERVATION_COUNT);
@@ -68,15 +70,15 @@ app.data = {
                     self.checklist_empty = false;
                 });
             }
-            self.fixWidth();
-        }, select_species: function (species) {
+            self.fixWidth(); // ensure table spacing is even for all quantities in the column
+        }, select_species: function (species) { // select species from dropdown
             let self = this;
             self.new_species = species.COMMON_NAME;
             self.showDropdown = false;
-            this.$nextTick(() => {
+            this.$nextTick(() => { // if selected from dropdown, move focus to quantity input
                 this.$refs.count.focus();
             });
-        }, update_quantity: function (idx, quantity) {
+        }, update_quantity: function (idx, quantity) { // update quantity of species
             let self = this;
             let species = this.checklist[idx];
             if (species.OBSERVATION_COUNT + quantity >= 0) {
@@ -89,8 +91,8 @@ app.data = {
                     console.log("updated", species.species_name, "quantity to", species.OBSERVATION_COUNT);
                 });
             }
-            self.fixWidth();
-        }, remove_item: function (idx) {
+            self.fixWidth(); // ensure table spacing is even for all quantities in the column
+        }, remove_item: function (idx) { // remove species from checklist
             let self = this;
             let species = this.checklist[idx];
             axios.post(remove_species_url, {
@@ -108,13 +110,14 @@ app.data = {
                     console.log('failed to delete species')
                 }
             });
-            self.fixWidth();
-        }, moveFocus: function (id) {
+            self.fixWidth(); // ensure table spacing is even for all quantities in the column
+        }, moveFocus: function (id) { // moves focus from current input field to specified input field
             this.$nextTick(() => {
                 this.$refs[id].focus();
             });
-        }, saveAndShowModal: function () {
+        }, saveAndShowModal: function () { // saves checklist in backend and shows modal/pop-up
             let self = this;
+            // checks for missing/invalid date and time
             if (self.observation_date == null || self.observation_date == "") {
                 self.valid_date = false;
             } else {
@@ -122,30 +125,30 @@ app.data = {
             }
             if (self.observation_time == null || self.observation_time == "") {
                 self.valid_time = false;
-            }  else {
+            } else {
                 self.valid_time = true;
             }
             if (self.valid_date == false || self.valid_time == false) {
                 return;
             }
             console.log(self.observation_date, self.observation_time, self.duration);
-            axios.post(save_checklist_url, {
+            axios.post(save_checklist_url, { // send to backend
                 event_id: self.event_id,
                 observation_date: self.observation_date,
                 observation_time: self.observation_time,
                 duration: self.duration
             }).then(function (r) {
             });
-            document.getElementById("submitModal").classList.add("is-active");
-            window.addEventListener('keydown', this.handleEscapeKey);
+            document.getElementById("submitModal").classList.add("is-active"); // show modal
+            window.addEventListener('keydown', this.handleEscapeKey); // activate escape key listener
         }, closeModal: function () {
-            document.getElementById("submitModal").classList.remove("is-active");
-            window.removeEventListener('keydown', this.handleEscapeKey);
+            document.getElementById("submitModal").classList.remove("is-active"); // hide modal
+            window.removeEventListener('keydown', this.handleEscapeKey); // remove escape key listener
         }, handleEscapeKey: function (e) {
             if (e.key === 'Escape') {
-                this.closeModal();
+                this.closeModal(); // call function to close modal if escape key is pressed
             }
-        }, handleClickOutside(event) {
+        }, handleClickOutside(event) { // if user clicks outside of dropdown, make dropdown go away
             const dropdown = this.$refs.dropdown;
             const input = this.$refs.species_input;
             if (dropdown && input) {
@@ -153,7 +156,7 @@ app.data = {
                     this.showDropdown = false;
                 }
             }
-        }, fixWidth: function () {
+        }, fixWidth: function () { // ensure table spacing is even for all quantities in the column
             let self = this;
             let width = '3ch';
             if (self.checklist.length > 0) {
@@ -161,14 +164,14 @@ app.data = {
                 width = `${Math.max(maxQuantity.toString().length, 3)}ch`;
             }
             self.max_char_width = width;
-        }, prevent_negatives: function (e) {
+        }, prevent_negatives: function (e) { // prevent user from typing negative numbers, decimals, or scientific notation
             if (e.key === '-' || e.key === '+' || e.key === 'e' || e.key === '.') {
                 e.preventDefault();
             }
         }
     },
     computed: {
-        filter_species() {
+        filter_species() { // search filter for species within checklist
             let self = this;
             if (this.search_species == "") {
                 return this.checklist;
@@ -179,7 +182,7 @@ app.data = {
                     return species.species_name.toLowerCase().includes(this.search_species.toLowerCase());
                 });
             }
-        }, show_species() {
+        }, show_species() { // search filter for all species to be added to checklist
             this.showDropdown = true;
             if (this.new_species.trim() === "") {
                 return [];
@@ -188,15 +191,16 @@ app.data = {
                 species.COMMON_NAME.toLowerCase().includes(this.new_species.toLowerCase())
             );
         }
-    }, mounted() {
+    }, mounted() { // add event listener for clicking outside of dropdown
         document.addEventListener('click', this.handleClickOutside);
-    }, beforeDestroy() {
+    }, beforeDestroy() { // remove event listener when component is destroyed
         document.removeEventListener('click', this.handleClickOutside);
     }
 };
 
 app.vue = Vue.createApp(app.data).mount("#app");
 
+// Load necessary data from backend on page load
 app.load_data = function () {
     axios.get(load_sightings_url).then(function (r) {
         app.vue.event_id = r.data.event_id;
